f3fd602e02a9cc3cd0154591206d5c0d2d17e7f9
naranburulov <naranburulov@gmail.com>
2023-02-19 14:26:03 -0500

    added filterChain() to SecurityConfig

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -2,12 +2,14 @@ package com.cydeo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,7 +31,28 @@ public class SecurityConfig {
                         Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
 
         return new InMemoryUserDetailsManager(userList);
-
     }
+
+    //everyone should be able to reach certain pages; without authentication - .antMatchers()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeRequests()                    //each request must be authorized
+                .antMatchers(
+                        "/",
+                        "/login",
+                        "/fragments/**",
+                        "/assets/**",
+                        "/images/**"
+                ).permitAll()                           //the above ones - permit without authentication
+                .anyRequest().authenticated()   //any other requests must be authenticated
+                .and()
+                .httpBasic()                    //temporary custom authentication form
+                .and().build();
+    }
+
+
+
 
 }
