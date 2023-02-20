314807b5ce7cae42e0083acea0f2400d694a45fc
naranburulov <naranburulov@gmail.com>
2023-02-20 17:53:26 -0500

    modified listAllTasksByStatus() and modified listAllTasksByStatusIsNot() at TaskServiceImpl

--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -12,6 +12,7 @@ import com.cydeo.repo.TaskRepository;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -103,7 +104,8 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasksByStatusIsNot(Status status) {
-        UserDTO loggedInUser = userService.findByUserName("john@employee.com");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserDTO loggedInUser = userService.findByUserName(username);
         List<Task> tasks = taskRepository.findAllByTaskStatusIsNotAndAssignedEmployee(status,
                 mapperUtil.convert(loggedInUser, new User()));
         return tasks.stream()
@@ -113,7 +115,8 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public List<TaskDTO> listAllTasksByStatus(Status status) {
-        UserDTO loggedInUser = userService.findByUserName("john@employee.com");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserDTO loggedInUser = userService.findByUserName(username);
         List<Task> tasks = taskRepository.findAllByTaskStatusAndAssignedEmployee(status,
                 mapperUtil.convert(loggedInUser, new User()));
         return tasks.stream()
